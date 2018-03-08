b16bf49ebc5ce38e6904e37b16add7238b4a1599
Seham <sehamsamy755@gmail.com>
2018-03-08 11:09:24 +0200

    login with validation

--- library/urls.py
+++ library/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from django.views.generic import RedirectView
-
+from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('branch/', include('branch.urls')),
     path('', RedirectView.as_view(url='/branch/')),
+   path('accounts/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 
 ]
