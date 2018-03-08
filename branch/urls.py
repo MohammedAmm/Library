9007899ed4c78d87a84f2544c1f57fcc5b56b92a
lw <https://github.com/Engy-Bakr?tab=repositories>
2018-03-08 10:55:08 +0200

    add writer with details

--- branch/urls.py
+++ branch/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('writers/',views.writers,name='writers'),
+    path('writer/<int:pk>', views.WriterDetailView.as_view(), name='writer-detail'),
+
+    
 
 ]
--- branch/views.py
+++ branch/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Writer,Book
+from django.views import generic
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
     return HttpResponse("Welcome to hotel transelvania")
+
+def writers(request):
+    writers=Writer.objects.all();
+    return render(request,'writers/list.html',{'writers':writers}); 
+
+class WriterDetailView(generic.DetailView):
+    model = Writer
