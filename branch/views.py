a2d6580d46f46c8e8499ba132db12b032af05262
Mohamed <mohammed.ashraaf.ma@gmail.com>
2018-03-08 15:31:23 +0200

    Registeration with validation && Design to pages

--- branch/views.py
+++ branch/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from .models import Writer, Book
+from .models import Writer,Book
 from django.views import generic
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
@@ -22,3 +25,8 @@ class WriterDetailView(generic.DetailView):
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
--- library/urls.py
+++ library/urls.py
@@ -16,14 +16,23 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.views.generic import RedirectView
 from django.views.generic.base import TemplateView
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import user_passes_test
+from branch import views
+from django.views.generic.base import RedirectView
+
+login_forbidden = user_passes_test(lambda u: u.is_anonymous,'/branch/index')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('branch/', include('branch.urls')),
     path('', RedirectView.as_view(url='/branch/')),
-   path('accounts/', include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('signup/', login_forbidden(views.SignUp.as_view()), name='signup'),
 
 ]
 
